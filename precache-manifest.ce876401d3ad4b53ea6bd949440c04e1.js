0ec9d978ef8d2512db0c58216ac7692c5a3a0003
Yuding Ai <yudingai@seas.upenn.edu>
2019-01-20 14:22:45 -0500

    Updates

---
+++ precache-manifest.ce876401d3ad4b53ea6bd949440c04e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/anglee-dashboard/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "11011c3fddc27ff57071",
+    "url": "/anglee-dashboard/static/js/runtime~main.11011c3f.js"
+  },
+  {
+    "revision": "b738bdc4728223898e4f",
+    "url": "/anglee-dashboard/static/js/main.b738bdc4.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/anglee-dashboard/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "b738bdc4728223898e4f",
+    "url": "/anglee-dashboard/static/css/main.17518d7c.chunk.css"
+  },
+  {
+    "revision": "0699093113865878498de3f9d537217b",
+    "url": "/anglee-dashboard/index.html"
+  }
+];
