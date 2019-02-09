97acffa2a5835a185a21a5c49818fb01e3895105
Yuding Ai <yudingai@seas.upenn.edu>
2019-02-09 18:15:20 -0500

    add nav bar toggle

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,7 +5,11 @@ import NavigationBrand from './NavigationBrand/NavigationBrand';
 const navigationItems = () => (
     <nav className='navbar navbar-expand-lg navbar-dark bg-dark' >
         <NavigationBrand link="/anglee-dashboard" exact> Ang Lee Dashboard </NavigationBrand>
-        <div className='navbar-collapse justify-content-end' >
+        <button className="navbar-toggler" type="button" data-toggle="collapse"
+            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className='collapse navbar-collapse justify-content-end' id='navbarSupportedContent' >
             <ul className='nav navbar-nav'>
                 <NavigationItem link="/anglee-dashboard/chicago" exact>Chicago</NavigationItem>
                 <NavigationItem link="/anglee-dashboard/newyork" exact>New York Times</NavigationItem>
