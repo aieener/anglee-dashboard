98af04db15d65688a1615a1c22fdacc47a7d33d1
Yuding Ai <yudingai@seas.upenn.edu>
2019-05-19 23:06:22 -0400

    set axios-core

--- anglee-dashboard-ui/src/axios-core.js
+++ anglee-dashboard-ui/src/axios-core.js
@@ -4,6 +4,4 @@ const instance = axios.create({
   baseURL: "https://anglee-dashboard-core.herokuapp.com"
 });
 
-instance.defaults.headers.common['Auth-Token'] = 'AUTH TOKEN FROM INSTANCE';
-
 export default instance;
