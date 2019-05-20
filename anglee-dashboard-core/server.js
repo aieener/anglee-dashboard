7ed08f2fdbabc9966d8e85f76c2391158c742376
Yuding Ai <yudingai@seas.upenn.edu>
2019-05-19 21:11:03 -0400

    add server.js

---
+++ anglee-dashboard-core/server.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const express = require("express");
+const port = process.env.PORT || 5000;
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dashboardRoutes = require('./routes/dashboard');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(dashboardRoutes);
+
+mongoose
+  .connect(
+    "mongodb+srv://yai:admin@angleecluster-agq8g.mongodb.net/dashboard?retryWrites=true"
+  )
+  .then(() => {
+    app.listen(port);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
