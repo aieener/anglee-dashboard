d0fa1870cde2cbb3cfc6e4b4dd21737469171e25
Yuding Ai <yudingai@seas.upenn.edu>
2019-05-26 22:30:23 -0400

    revert and fix bug

--- anglee-dashboard-ui/src/axios/axios-core.js
+++ anglee-dashboard-ui/src/axios/axios-core.js
@@ -3,6 +3,4 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: "https://anglee-dashboard-core.herokuapp.com"
 });
-const instance = axios.create();
-
 export default instance;
